34022d23375a5cfa47fcfdfd70f39eedb00c4684
Maria Eduarda <87625092+maririchii@users.noreply.github.com>
2025-04-30 19:53:16 -0300

    Initial commit

---
+++ src/main/java/com/project/socc/SoccApplication.java
@@ -0,0 +1,13 @@
+package com.project.socc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccApplication.class, args);
+	}
+
+}
