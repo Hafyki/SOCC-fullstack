a47c1cfe5c9d7aefc86f9501c8ceecc75956073a
Hafyki <hafymjck@gmail.com>
2025-05-12 14:58:04 -0300

    Added the GET ALL, GET ONE and PATCH methods to the UserController.java file.

--- src/main/java/com/project/socc/controllers/UserController.java
+++ src/main/java/com/project/socc/controllers/UserController.java
@@ -1,19 +1,26 @@
 package com.project.socc.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping("/users")
 public class UserController {
 
+    //POST
     @PostMapping
     public String postUser() {
         return "POST User created";
     }
 
-    // GET
+    //GET ALL
+    @GetMapping
+    public String getAllUsers() { return "GET Users returned"; }
 
-    // PATCH
+    //GET ONE
+    @GetMapping("{/id}")
+    public String getUser(@PathVariable int id) { return "GET User with id:" + id +" returned"; }
+
+    //PATCH
+    @PatchMapping("/{id}")
+    public String patchUser(@PathVariable int id) { return "User with id: " + id + " patched"; }
 }
